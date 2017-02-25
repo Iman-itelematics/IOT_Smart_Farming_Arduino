b41b5268f8e4230b44c124e1d601baaa13f8c58a
KasunGunarathna <kasunpradeepa.gunarathna@gmail.com>
2017-02-24 16:25:36 -0800

    graphs and waterpump fix commit

--- add.php
+++ add.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+
    	include("connect.php");
    	
    	$link=Connection();
@@ -12,13 +13,12 @@ session_start();
 	$soil1=$_POST["soil1"];
 	$soilTemp1=$_POST["soilTemp1"];
 
-
 	$query = "INSERT INTO `tempLog` (`temperature`, `humidity`,`tempr`,`presser`,`pressera`,`soilmoisture`,`soiltemperature`) 
 		VALUES ('".$temp1."','".$hum1."','".$tempr1."','".$press1."','".$press01."','".$soil1."','".$soilTemp1."')"; 
-   	
 
    	mysql_query($query,$link);
 	mysql_close($link);
 
    	header("Location: index.php");
+
 ?>
